bec925ddbd95844b7426b528c03ace5a483e407b
Risav Katara <87906280+WorthySteve007@users.noreply.github.com>
2022-07-15 00:40:30 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Header from "./Header";
+import AddContacts from "./AddContact";
+import ContactList from "./ContactList";
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
